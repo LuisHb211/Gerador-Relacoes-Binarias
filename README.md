df8ef669b615fbbf197490e10d487bdb1dd17da7
Luis Henrique Bonini <luishbsantiago@hotmail.com>
2023-03-21 21:31:53 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+O programa tem como intuito gerar as relacoes binarias,
+a partir de um valor de itens presentes no conjunto.
+
+Lembrando que a quantidade de relacoes binarias possiveis segue a formula: 2^(n*n), sendo 'n' a quantidade de elementos de um cojunto.
+
+O principal propósito foi analisar o andamento do programa com uma quantidade de 5 elementos, gerando assim 33.553.432 relacoes binarias possiveis.
+Com uma quantidade de 5 elementos no cojunto foi analisado um tempo de x segundos para a realizacao do programa.
+
+Nesse programa foi utilizado conceitos sobre:
+1- Conjuntos
+2- Classificacao de conjuntos
+3- Python
+4- Numeros binarios
+5- bitwise operator
---
+++ geradorRelacoes.py
@@ -0,0 +1,101 @@
+import time
+
+def pot_conj(n):
+    pot = []
+    for subconj in range(2**(n*n)):
+        pot.append(conjPares(subconj,n))
+    return pot
+    
+def conjPares(s, n):
+    conj = []
+    for i in range(n):
+        for j in range(n):
+            if bitLigado(i,j,n,s):
+                conj.append((i+1, j+1))
+    return conj
+
+def bitLigado(i,j,n,s):
+    return s&(1<<(i*n+j))!=0
+
+def simetrica(subconj):
+    for a, b in subconj:
+        if (b, a) not in subconj:
+            return False
+    return True
+
+def transitiva(subconj):
+    for a, b in subconj:
+        for c, d in subconj:
+            if b == c and (a, d) not in subconj:
+                return False
+    return True
+
+def reflexiva(subconj, n):
+    for i in range(1, n+1):
+        if (i, i) not in subconj:
+            return False
+    return True
+
+def irreflexiva(subconj):
+    for a, b in subconj:
+        if a == b:
+            return False
+    return True
+
+def equivalente(subconj, n):
+    return simetrica(subconj) and transitiva(subconj) and reflexiva(subconj, n)
+
+def injetora(subconj):
+    valores_imagem = set()
+    for _, imagem in subconj:
+        if imagem in valores_imagem:
+            return False
+        valores_imagem.add(imagem)
+    return True
+
+def sobrejetora(subconj, n):
+    valores_imagem = set()
+    for _, imagem in subconj:
+        valores_imagem.add(imagem)
+    return len(valores_imagem) == n
+
+def bijetora(subconj, n):
+    return injetora(subconj) and sobrejetora(subconj, n)
+
+def gerar_caracteristicas(n):
+    resultado = pot_conj(n)
+    caracteristicas = []
+    for subconj in resultado:
+        car = ''
+        if simetrica(subconj):
+            car += 'S'
+        if transitiva(subconj):
+            car += 'T'
+        if reflexiva(subconj, n):
+            car += 'R'
+        if irreflexiva(subconj):
+            car += 'I'
+        if equivalente(subconj, n):
+            car += 'E'
+        if injetora(subconj):
+            car += 'Fi'
+        if sobrejetora(subconj, n):
+            car += 'Fs'
+        if bijetora(subconj, n):
+            car += 'Fb'
+        caracteristicas.append((list(subconj), car))
+    return caracteristicas
+
+caracteristicas = gerar_caracteristicas(int(input("Qual a quantidade de elementos no conjunto?")))
+
+inicio = time.time()
+
+with open('resultados.txt', 'w') as arquivo:
+    for subconj, car in caracteristicas:
+        arquivo.write(str(subconj) + ' ' + car + '\n')
+
+fim = time.time()
+tempoTotal = fim - inicio
+
+print('O tempo para execucao foi de {:.3f}s'.format(tempoTotal))
+parar = input("Aparte qualquer tecla para sair")
